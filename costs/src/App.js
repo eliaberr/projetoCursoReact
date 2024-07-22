ec85d007e4ca84b4c949c5040af94ab30325b0c7
Eliabr Rodrigues <eliabe859@gmail.com>
2024-07-22 08:12:37 -0300

    Finalizado a aula 18

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,6 +3,7 @@ import Home from './Components/Pages/Home';
 import Empresa from './Components/Pages/Empresa';
 import Contato from './Components/Pages/Contato';
 import NemProject from './Components/Pages/NemProject';
+import Container from './Components/layont/Container';
 
 function App() {
   return (
@@ -13,12 +14,15 @@ function App() {
         <Link to="/empresa">Empresa</Link>
         <Link to="/newProject">Novo Projeto</Link>
       </div>
-      <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route path='/contato' element={<Contato/>} />
-        <Route path='/empresa' element={<Empresa/>}/>
-        <Route path='/newProject' element={<NemProject/>}/>
-      </Routes>
+      <Container customClass="min-heigth">
+        <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route path='/contato' element={<Contato/>} />
+            <Route path='/empresa' element={<Empresa/>}/>
+            <Route path='/newProject' element={<NemProject/>}/>
+        </Routes>
+      </Container>
+      <p>footer</p>
     </Router>
   );
 }
